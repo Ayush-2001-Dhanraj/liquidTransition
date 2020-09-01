38106b4258161dbb5b7b7ccc4ff885873f8cc740
Ayush Dhanraj <51990367+Ayush-2001-Dhanraj@users.noreply.github.com>
2020-09-01 20:59:14 +0530

    Add to initial repo

---
+++ app.js
@@ -0,0 +1,10 @@
+
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.1,
+  image1: "./images/image1.jpg",
+  image2: "./images/image3.jpg",
+  angle1 : Math.PI / 6,
+  angle2 : -Math.PI / 6 * 3,
+  displacementImage: "./images/diss.png"
+});
